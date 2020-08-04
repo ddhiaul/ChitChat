d055e910eafc5d4176d7243bc5c56f6a15f41daa
Dhiyaul Aulia <dhiyaul.aulia2003@gmail.com>
2020-08-04 15:53:34 +0700

    Manifest, nambah bbrp drawable, nambah bbrp activity

---
+++ app/src/main/java/com/aulia/idn/chitchat/VisitProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.aulia.idn.chitchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VisitProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visit_profile)
+    }
+}
